15c95a200f81857a48e0b222323b4e87b2503652
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-11-05 19:12:10 +0200

    Video controller middleware, Index and store

--- backend-laravel-sanctum-api/app/Http/Controllers/VideoController.php
+++ backend-laravel-sanctum-api/app/Http/Controllers/VideoController.php
@@ -4,44 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class VideoController extends Controller
+class VideoController extends Controller implements HasMiddleware
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public static function middleware()
+    {
+        return [
+            new Middleware('auth:sanctum', except: []),
+        ];
+    }
     public function index()
     {
-        //
+        $videos = Video::with('user')->get(); // Fetch videos with associated user data
+        return response()->json($videos);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        // Validate incoming request data
+        $fields = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            // 'url' => 'required|url',
+            'url' => 'required',
+        ]);
+
+        $video = $request->user()->videos()->create($fields);
+
+        return response()->json($video, 201); 
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Video $video)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Video $video)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+        // Update the video
+        $video->update($request->only(['title', 'description', 'url']));
+
+        return response()->json($video); 
+    }
+
     public function destroy(Video $video)
     {
         //
--- backend-laravel-sanctum-api/routes/api.php
+++ backend-laravel-sanctum-api/routes/api.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VideoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('videos', VideoController::class);
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
