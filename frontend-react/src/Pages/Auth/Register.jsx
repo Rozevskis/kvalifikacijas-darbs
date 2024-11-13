91a3fb085d4c12fd85821007e8b1f54436388ea2
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-11-13 12:19:07 +0200

    Register errors are display

--- frontend-react/src/Pages/Auth/Register.jsx
+++ frontend-react/src/Pages/Auth/Register.jsx
@@ -7,6 +7,9 @@ export default function Register() {
     password: "",
     password_confirmation: "",
   });
+
+  const [errors, setErrors] = useState({});
+
   async function handleRegister(e) {
     e.preventDefault();
     const res = await fetch("/api/register", {
@@ -14,41 +17,62 @@ export default function Register() {
       body: JSON.stringify(formData),
     });
     const data = await res.json();
-    console.log(data);
+
+    if (data.errors) {
+      setErrors(data.errors);
+    } else {
+      console.log(data);
+    }
   }
 
   return (
     <div>
       <h1 className="title">Register</h1>
       <form onSubmit={handleRegister} className="w-1/2 space-y-4 mx-auto ">
-        <input
-          type="text"
-          placeholder="Name"
-          value={formData.name}
-          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-        />
-        <input
-          type="password"
-          placeholder="Password confirmation"
-          value={formData.password_confirmation}
-          onChange={(e) =>
-            setFormData({ ...formData, password_confirmation: e.target.value })
-          }
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
+          {errors.name && <p className="error">{errors.name}</p>}
+        </div>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+          {errors.password && <p className="error">{errors.password}</p>}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password confirmation"
+            value={formData.password_confirmation}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                password_confirmation: e.target.value,
+              })
+            }
+          />
+        </div>
         <button className="primary-btn">Register</button>
       </form>
     </div>
