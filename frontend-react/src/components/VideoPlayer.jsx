ec222025c07c5e36f1cddd9d235eab90761a8657
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-12-06 22:16:46 +0200

    autoplay for video

--- frontend-react/src/components/VideoPlayer.jsx
+++ frontend-react/src/components/VideoPlayer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const VideoPlayer = ({ videoSrc }) => {
   return (
     <div className="video-container">
-      <video controls width="100%" height="auto">
+      <video autoplay controls width="100%" height="auto">
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
