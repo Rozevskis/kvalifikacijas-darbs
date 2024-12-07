5b0409a7f0514458e2da2942f85fe5efbcb495d9
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-12-07 21:59:39 +0200

    Move Video list to blocks

--- frontend-react/src/Pages/Home.jsx
+++ frontend-react/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import VideoList from "../components/VideoList"; // Assuming it's in the components folder
+import VideoList from "../components/Blocks/VideoList"; // Assuming it's in the components folder
 
 export default function Home() {
   return <VideoList />;
--- frontend-react/src/components/Blocks/VideoList.jsx
+++ frontend-react/src/components/Blocks/VideoList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import VideoCard from "../components/VideoCard";
+import VideoCard from "../VideoCard";
 
 const VideoList = () => {
   const [videos, setVideos] = useState([]);
