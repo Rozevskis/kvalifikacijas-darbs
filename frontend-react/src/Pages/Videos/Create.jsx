a59c722f838636be860b28a227c74c44063080ef
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-12-13 22:48:08 +0200

    Updated VIdeo creation, returns error

--- frontend-react/src/Pages/Videos/Create.jsx
+++ frontend-react/src/Pages/Videos/Create.jsx
@@ -6,6 +6,7 @@ import VideoUploader from "../../components/VideoUploader";
 export default function Create() {
   const navigate = useNavigate();
   const { token } = useContext(AppContext);
+  const [uploading, setUploading] = useState(true);
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -39,35 +40,41 @@ export default function Create() {
   return (
     <>
       <h1 className="title">Upload a new video</h1>
-      <form onSubmit={handleCreate} className="w-1/2 mx-auto space-y-6">
-        <div>
-          <VideoUploader onUploadComplete={handleVideoUploadComplete} />
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Video title"
-            value={formData.title}
-            onChange={(e) =>
-              setFormData({ ...formData, title: e.target.value })
-            }
-          />
-          {errors.title && <p className="text-red-500">{errors.title}</p>}
-        </div>
-        <div>
-          <textarea
-            placeholder="Description"
-            value={formData.description}
-            onChange={(e) =>
-              setFormData({ ...formData, description: e.target.value })
-            }
-          />
-          {errors.description && (
-            <p className="text-red-500">{errors.description}</p>
-          )}
-        </div>
-        <button className="primary-btn">Post</button>
-      </form>
+      <div className="">
+        <VideoUploader
+          onUploadComplete={handleVideoUploadComplete}
+          uploading={uploading}
+          setUploading={setUploading}
+        />
+      </div>
+      {!uploading && (
+        <form onSubmit={handleCreate} className="w-1/2 mx-auto space-y-6">
+          <div>
+            <input
+              type="text"
+              placeholder="Video title"
+              value={formData.title}
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+            />
+            {errors.title && <p className="text-red-500">{errors.title}</p>}
+          </div>
+          <div>
+            <textarea
+              placeholder="Description"
+              value={formData.description}
+              onChange={(e) =>
+                setFormData({ ...formData, description: e.target.value })
+              }
+            />
+            {errors.description && (
+              <p className="text-red-500">{errors.description}</p>
+            )}
+          </div>
+          <button className="primary-btn">Post</button>
+        </form>
+      )}
     </>
   );
 }
--- frontend-react/src/components/VideoUploader.jsx
+++ frontend-react/src/components/VideoUploader.jsx
@@ -2,13 +2,11 @@ import React, { useRef, useState } from "react";
 import axios from "axios";
 import { VideoPlayer } from "./VideoPlayer";
 import "./VideoUploader.css";
-
-const VideoUploader = ({ onUploadComplete }) => {
+const VideoUploader = ({ onUploadComplete, uploading, setUploading }) => {
   const [filePreview, setFilePreview] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const fileInputRef = useRef(null);
   const [uploadedVideoUrl, setUploadedVideoUrl] = useState(null);
-  const [uploading, setUploading] = useState(false);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -34,7 +32,6 @@ const VideoUploader = ({ onUploadComplete }) => {
     formData.append("file", file);
 
     try {
-      setUploading(true);
       const response = await axios.post(
         "http://127.0.0.1:8000/api/videos/upload",
         formData,
@@ -49,8 +46,10 @@ const VideoUploader = ({ onUploadComplete }) => {
       setUploadedVideoUrl(videoUrl);
       setFilePreview(videoUrl); // Show the uploaded video
       onUploadComplete(videoUrl); // Pass the URL back to the parent component
+      return true;
     } catch (error) {
       setErrorMessage("Failed to upload video. Please try again.");
+      return false;
     } finally {
       setUploading(false);
     }
@@ -71,34 +70,33 @@ const VideoUploader = ({ onUploadComplete }) => {
   };
 
   return (
-    <div className="uploader">
+    <div className="max-w-[900px] h-[600px] mx-auto">
       <label
         htmlFor="file-upload"
         id="file-drag"
         onDragOver={handleDragOver}
         onDrop={handleDrop}
-        className={filePreview ? "hover" : ""}
+        className={`group flex flex-col justify-center items-center w-full h-full p-8 text-center bg-slate-50 hover:bg-slate-200 hover:border-0 rounded-lg ${
+          !filePreview ? "border-2 border-gray-300" : ""
+        } transition-all duration-200 cursor-pointer select-none`}
       >
         {filePreview ? (
           <VideoPlayer videoSrc={uploadedVideoUrl || filePreview} />
         ) : (
-          <div id="start ">
-            <i className="fa fa-download" aria-hidden="true"></i>
-            <p className="text-blue-300 text-sm">~ 200MB max</p>
-            <div>Select a file or drag here</div>
+          <div id="start" className="flex flex-col items-center">
+            <i
+              id="upload-icon"
+              className="fa fa-download text-5xl mb-4 group-hover:text-7xl group-hover:text-slate-600 "
+              aria-hidden="true"
+            ></i>
+            <p className="text-blue-300 text-sm group-hover:hidden">
+              ~ 200MB max
+            </p>
+            <div className="text-gray-600 group-hover:hidden">
+              Select a file or drag here
+            </div>
             {errorMessage && (
-              <div className="text-red-500 font-bold">{errorMessage}</div>
-            )}
-            {uploading ? (
-              <div>Uploading...</div>
-            ) : (
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => fileInputRef.current.click()}
-              >
-                Select a file
-              </button>
+              <div className="text-red-500 font-bold mt-2">{errorMessage}</div>
             )}
           </div>
         )}
@@ -110,7 +108,7 @@ const VideoUploader = ({ onUploadComplete }) => {
         accept="video/*"
         onChange={handleFileChange}
         ref={fileInputRef}
-        style={{ display: "none" }}
+        className="hidden"
       />
     </div>
   );
